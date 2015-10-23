91196a9a2499d0a4f52410c980e8c4387c0e15f5
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2015-10-23 14:49:36 +0330

    Fix language problem

--- GMap.NET.WindowsForms/GMap.NET.WindowsForms/MapPrintPreviewForm.designer.cs
+++ GMap.NET.WindowsForms/GMap.NET.WindowsForms/MapPrintPreviewForm.designer.cs
@@ -39,18 +39,18 @@ namespace GMap.NET.WindowsForms
             this.dlgPageSetup = new System.Windows.Forms.PageSetupDialog();
             this.ScrollPanel = new System.Windows.Forms.Panel();
             this.menu = new System.Windows.Forms.ToolStrip();
-            this.lblSpace2 = new System.Windows.Forms.ToolStripLabel();
-            this.lblSpace = new System.Windows.Forms.ToolStripLabel();
-            this.lblSpace3 = new System.Windows.Forms.ToolStripLabel();
-            this.lblSpace5 = new System.Windows.Forms.ToolStripLabel();
-            this.lblSpace4 = new System.Windows.Forms.ToolStripLabel();
-            this.prgTileDownloadStatus = new System.Windows.Forms.ToolStripProgressBar();
             this.btnPrint = new System.Windows.Forms.ToolStripButton();
+            this.lblSpace2 = new System.Windows.Forms.ToolStripLabel();
             this.cmbPaperSizes = new System.Windows.Forms.ToolStripDropDownButton();
             this.btnClose = new System.Windows.Forms.ToolStripButton();
+            this.lblSpace = new System.Windows.Forms.ToolStripLabel();
             this.btnChangeMapColor = new System.Windows.Forms.ToolStripButton();
+            this.lblSpace3 = new System.Windows.Forms.ToolStripLabel();
             this.btnRotate = new System.Windows.Forms.ToolStripButton();
+            this.lblSpace5 = new System.Windows.Forms.ToolStripLabel();
             this.btnShowHideMarkerInfo = new System.Windows.Forms.ToolStripButton();
+            this.lblSpace4 = new System.Windows.Forms.ToolStripLabel();
+            this.prgTileDownloadStatus = new System.Windows.Forms.ToolStripProgressBar();
             this.MapPanel.SuspendLayout();
             this.ScrollPanel.SuspendLayout();
             this.menu.SuspendLayout();
@@ -126,6 +126,7 @@ namespace GMap.NET.WindowsForms
             // 
             this.menu.CanOverflow = false;
             this.menu.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.menu.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.btnPrint,
             this.lblSpace2,
@@ -145,12 +146,53 @@ namespace GMap.NET.WindowsForms
             this.menu.Size = new System.Drawing.Size(1101, 74);
             this.menu.TabIndex = 6;
             // 
+            // btnPrint
+            // 
+            this.btnPrint.AutoSize = false;
+            this.btnPrint.Image = global::System.Windows.Forms.Properties.Resources.MapPrinter;
+            this.btnPrint.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnPrint.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never;
+            this.btnPrint.Size = new System.Drawing.Size(75, 71);
+            this.btnPrint.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnPrint.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
+            this.btnPrint.TextImageRelation = System.Windows.Forms.TextImageRelation.Overlay;
+            this.btnPrint.ToolTipText = "Print Map";
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
             // lblSpace2
             // 
             this.lblSpace2.AutoSize = false;
             this.lblSpace2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
             this.lblSpace2.Name = "lblSpace2";
             this.lblSpace2.Size = new System.Drawing.Size(30, 71);
+            // 
+            // cmbPaperSizes
+            // 
+            this.cmbPaperSizes.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbPaperSizes.Image = global::System.Windows.Forms.Properties.Resources.Document_xml_icon;
+            this.cmbPaperSizes.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cmbPaperSizes.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.cmbPaperSizes.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmbPaperSizes.Name = "cmbPaperSizes";
+            this.cmbPaperSizes.Size = new System.Drawing.Size(123, 71);
+            this.cmbPaperSizes.Text = "A4";
+            this.cmbPaperSizes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.cmbPaperSizes.ToolTipText = "Paper Type";
+            this.cmbPaperSizes.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.cmbPaperSizes_DropDownItemClicked);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.btnClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnClose.Image = global::System.Windows.Forms.Properties.Resources.close_icon;
+            this.btnClose.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnClose.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(68, 71);
+            this.btnClose.ToolTipText = "Cancel";
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // lblSpace
             // 
@@ -159,6 +201,17 @@ namespace GMap.NET.WindowsForms
             this.lblSpace.Name = "lblSpace";
             this.lblSpace.Size = new System.Drawing.Size(30, 71);
             // 
+            // btnChangeMapColor
+            // 
+            this.btnChangeMapColor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnChangeMapColor.Image = global::System.Windows.Forms.Properties.Resources.MapPrintPreview;
+            this.btnChangeMapColor.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnChangeMapColor.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnChangeMapColor.Name = "btnChangeMapColor";
+            this.btnChangeMapColor.Size = new System.Drawing.Size(63, 71);
+            this.btnChangeMapColor.ToolTipText = "BlackWhite / Colored";
+            this.btnChangeMapColor.Click += new System.EventHandler(this.btnChangeMapColor_Click);
+            // 
             // lblSpace3
             // 
             this.lblSpace3.AutoSize = false;
@@ -166,12 +219,34 @@ namespace GMap.NET.WindowsForms
             this.lblSpace3.Name = "lblSpace3";
             this.lblSpace3.Size = new System.Drawing.Size(30, 71);
             // 
+            // btnRotate
+            // 
+            this.btnRotate.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnRotate.Image = global::System.Windows.Forms.Properties.Resources.Rotate2Portrait;
+            this.btnRotate.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnRotate.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnRotate.Name = "btnRotate";
+            this.btnRotate.Size = new System.Drawing.Size(68, 71);
+            this.btnRotate.ToolTipText = "Rotate Page";
+            this.btnRotate.Click += new System.EventHandler(this.btnRotate_Click);
+            // 
             // lblSpace5
             // 
             this.lblSpace5.AutoSize = false;
             this.lblSpace5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
             this.lblSpace5.Name = "lblSpace5";
             this.lblSpace5.Size = new System.Drawing.Size(30, 71);
+            // 
+            // btnShowHideMarkerInfo
+            // 
+            this.btnShowHideMarkerInfo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnShowHideMarkerInfo.Image = global::System.Windows.Forms.Properties.Resources.ShowInfo;
+            this.btnShowHideMarkerInfo.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnShowHideMarkerInfo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnShowHideMarkerInfo.Name = "btnShowHideMarkerInfo";
+            this.btnShowHideMarkerInfo.Size = new System.Drawing.Size(68, 71);
+            this.btnShowHideMarkerInfo.ToolTipText = "Hide/Show Tooltips";
+            this.btnShowHideMarkerInfo.Click += new System.EventHandler(this.btnShowHideMarkerInfo_Click);
             // 
             // lblSpace4
             // 
@@ -187,81 +262,7 @@ namespace GMap.NET.WindowsForms
             this.prgTileDownloadStatus.Name = "prgTileDownloadStatus";
             this.prgTileDownloadStatus.Size = new System.Drawing.Size(300, 40);
             this.prgTileDownloadStatus.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
-            this.prgTileDownloadStatus.ToolTipText = "در حال بار گذاری نقشه";
-            // 
-            // btnPrint
-            // 
-            this.btnPrint.AutoSize = false;
-            this.btnPrint.Image = global::System.Windows.Forms.Properties.Resources.MapPrinter;
-            this.btnPrint.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.btnPrint.ImageTransparentColor = System.Drawing.Color.Transparent;
-            this.btnPrint.Name = "btnPrint";
-            this.btnPrint.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never;
-            this.btnPrint.Size = new System.Drawing.Size(75, 71);
-            this.btnPrint.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnPrint.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
-            this.btnPrint.TextImageRelation = System.Windows.Forms.TextImageRelation.Overlay;
-            this.btnPrint.ToolTipText = "چاپ نقشه";
-            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
-            // 
-            // cmbPaperSizes
-            // 
-            this.cmbPaperSizes.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmbPaperSizes.Image = global::System.Windows.Forms.Properties.Resources.Document_xml_icon;
-            this.cmbPaperSizes.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.cmbPaperSizes.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.cmbPaperSizes.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.cmbPaperSizes.Name = "cmbPaperSizes";
-            this.cmbPaperSizes.Size = new System.Drawing.Size(111, 71);
-            this.cmbPaperSizes.Text = "A4";
-            this.cmbPaperSizes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.cmbPaperSizes.ToolTipText = "نوع صفحه";
-            this.cmbPaperSizes.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.cmbPaperSizes_DropDownItemClicked);
-            // 
-            // btnClose
-            // 
-            this.btnClose.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.btnClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnClose.Image = global::System.Windows.Forms.Properties.Resources.close_icon;
-            this.btnClose.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.btnClose.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(68, 71);
-            this.btnClose.ToolTipText = "انصراف از چاپ";
-            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
-            // 
-            // btnChangeMapColor
-            // 
-            this.btnChangeMapColor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnChangeMapColor.Image = global::System.Windows.Forms.Properties.Resources.MapPrintPreview;
-            this.btnChangeMapColor.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.btnChangeMapColor.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnChangeMapColor.Name = "btnChangeMapColor";
-            this.btnChangeMapColor.Size = new System.Drawing.Size(63, 71);
-            this.btnChangeMapColor.ToolTipText = "سیاه و سفید / رنگی";
-            this.btnChangeMapColor.Click += new System.EventHandler(this.btnChangeMapColor_Click);
-            // 
-            // btnRotate
-            // 
-            this.btnRotate.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnRotate.Image = global::System.Windows.Forms.Properties.Resources.Rotate2Portrait;
-            this.btnRotate.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.btnRotate.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnRotate.Name = "btnRotate";
-            this.btnRotate.Size = new System.Drawing.Size(68, 71);
-            this.btnRotate.ToolTipText = "چرخاندن صفحه";
-            this.btnRotate.Click += new System.EventHandler(this.btnRotate_Click);
-            // 
-            // btnShowHideMarkerInfo
-            // 
-            this.btnShowHideMarkerInfo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnShowHideMarkerInfo.Image = global::System.Windows.Forms.Properties.Resources.ShowInfo;
-            this.btnShowHideMarkerInfo.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.btnShowHideMarkerInfo.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnShowHideMarkerInfo.Name = "btnShowHideMarkerInfo";
-            this.btnShowHideMarkerInfo.Size = new System.Drawing.Size(68, 71);
-            this.btnShowHideMarkerInfo.ToolTipText = "نمایش / عدم نمایش بالون اطلاعات مشتری";
-            this.btnShowHideMarkerInfo.Click += new System.EventHandler(this.btnShowHideMarkerInfo_Click);
+            this.prgTileDownloadStatus.ToolTipText = "Map processing . . .";
             // 
             // MapPrintPreviewForm
             // 
@@ -273,7 +274,7 @@ namespace GMap.NET.WindowsForms
             this.Controls.Add(this.ScrollPanel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MapPrintPreviewForm";
-            this.Text = "پیش نمایش چاپ";
+            this.Text = "Map Print Preview";
             this.MapPanel.ResumeLayout(false);
             this.ScrollPanel.ResumeLayout(false);
             this.menu.ResumeLayout(false);
